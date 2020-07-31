46c7531973dcc98f0dba7126711702acb477b8dc
F-19-F <58457605+F-19-F@users.noreply.github.com>
2020-07-31 23:18:22 +0800

    更新model中print的逻辑

--- game.cpp
+++ game.cpp
@@ -16,7 +16,7 @@ void size()
     //test
     //程序输出有两种方式：一种是即时处理方式，另一种是先暂存起来，然后再大块写入的方式，前者往往造成较高的系统负担，实测关闭buf的cpu占用是开启的一倍
     //故没有采用直接关闭buf
-/*int main()
+int main()
 {
     int key_signal=0;
     int Press_times=0;
@@ -29,34 +29,16 @@ void size()
     model *b;
     for (int i=1;i<=5;i++)
     {
-        //a.debug_core();
-        //a.clean();
-        //key_run=0;
         b=new model(2);
         a.Add_model(b,&key_signal,&Press_times);
         //Press_times=0;
         delete b;
-        //key_lock.lock();
-        //key_run=1;
-        //Press_times=0;
-        //key_lock.unlock();
     }
-    //model b(5);
-    //a.Add_model(&b);
     cout<<endl;
-   // key_run=-1;
-    /**********直接getchar()行不通
-    while (1)
-    {
-        std::cout<<getchar();
-    }
-    
-    //std::thread t1(ky);
-    //t1.detach();
-    //t1.~thread();
     return 0;
-}*/
-int main()
+}
+//model测试代码
+/*int main()
 {
     model* b;
     int j=4;
@@ -78,4 +60,4 @@ int main()
         delete b;
     }
     return 0;
-}
+}*/
