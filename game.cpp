4e1f4c37374622feee48806fb63cff5c6b97065a
F-19-F <1767306012@qq.com>
2020-08-01 17:01:17 +0800

    重构model和keydec的代码

--- game.cpp
+++ game.cpp
@@ -2,62 +2,35 @@
 #include <thread>
 #include <mutex>
 #include <unistd.h>
-#include <sys/ioctl.h>//linux/unix
-#include "include/terminal-linux.h"//linux/unix
+#include <sys/ioctl.h>              //linux/unix
+#include "include/terminal-linux.h" //linux/unix
 #include "include/game_core.h"
 #include "include/size.h"
 #include "include/keydec.h"
-bool changed = false;
 using namespace std;
-void size()
-{
-    Size_detecter(&changed);
-}
-    //test
-    //程序输出有两种方式：一种是即时处理方式，另一种是先暂存起来，然后再大块写入的方式，前者往往造成较高的系统负担，实测关闭buf的cpu占用是开启的一倍
-    //故没有采用直接关闭buf
 int main()
 {
-    int key_signal=0;
-    int Press_times=0;
-    //int key_run=-1;
-    //std::thread t1(getkey,&key_signal,&Press_times,&key_run);
-    //t1.detach();
+    /*Key_dec a;
+    //key_base temp;
+    int temp;
+    a.start();
+    int c=10;
+    while (1)
+    {
+        temp = a.pop();
+        if (temp!= 0)
+        {
+            cout << temp<< endl;
+            if (temp==up)
+            break;
+        }
+    }
+    a.stop();
+    this_thread::sleep_for(std::chrono::milliseconds(200));*/
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    game_core a(w.ws_row, (w.ws_col / 3) * 2,1);
-    model *b;
-    for (int i=1;i<=5;i++)
-    {
-        b=new model(2);
-        a.Add_model(b,&key_signal,&Press_times);
-        //Press_times=0;
-        delete b;
-    }
-    cout<<endl;
+    model a(1);
+    game_core b(w.ws_row,w.ws_col,1);
+    b.Add_model(&a);
     return 0;
 }
-//model测试代码
-/*int main()
-{
-    model* b;
-    int j=4;
-    for (int i=1;i<=5;i++)
-    {
-        b= new model(i);
-        do
-        {
-            b->get_temp();
-            b->print_row(1);
-            moveright(10);
-            cout<<"height"<<b->height<<"length"<<b->length;
-            b->changer_neg(90);
-            cout<<endl;
-            this_thread::sleep_for(std::chrono::milliseconds(200));
-
-        } while (--j);
-        j=4;
-        delete b;
-    }
-    return 0;
-}*/
--- include/keydec.h
+++ include/keydec.h
@@ -4,4 +4,25 @@
 #define down 66
 #define right 67
 #define left 68
-int getkey(int* output,int* times,int* reset);
+//定义一个按键缓冲区，getkey得到的按键值存储在其中，方块下落时从中取出数据来响应
+struct key_base
+{
+    int signal;
+    key_base *next;
+};
+class Key_dec
+{
+private:
+    key_base *first;
+    key_base *last;
+
+public:
+    int psignal;
+    Key_dec();
+    ~Key_dec();
+    void start();
+    void stop();
+    int push(int signal);
+    int pop();
+    void clean();
+};
