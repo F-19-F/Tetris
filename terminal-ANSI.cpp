ffd7f94f57cc4060f775ae927e463e80e4b7f913
F-19-F <58457605+F-19-F@users.noreply.github.com>
2020-08-15 22:49:56 +0800

    Add color support for POSIX

---
+++ terminal-ANSI.cpp
@@ -0,0 +1,30 @@
+#ifndef _WIN32
+#include"include/ANSI_control.hpp"
+#include"include/model.hpp"
+#include<iostream>
+using namespace std;
+int Temp[6][3]={Random_Color_1,Random_Color_2,Random_Color_3,Random_Color_4,Random_Color_5,Random_Color_6};
+void Set_Default_color(int r,int g,int b,int f_r,int f_g,int f_b)
+{
+    cout<<"\033[48;2;"<<r<<";"<<g<<";"<<b<<";m";//设置背景色
+    clean_screen();
+    cout<<"\033[38;2;"<<f_r<<";"<<f_g<<";"<<f_b<<";m";//设置前景色
+}
+void color(int i)
+{
+    if (i>7)
+    {
+        return;
+    }
+    cout<<"\033[38;2;";
+    for (int j=0;j<3;j++)
+    {
+        cout<<Temp[i-1][j]<<";";
+    }
+    cout<<"m";
+}
+void Reset_color()
+{
+    end_all();
+}
+#endif
