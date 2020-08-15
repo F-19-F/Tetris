945ccb89822096744d56a7281f839cf9faee68b7
F-19-F <58457605+F-19-F@users.noreply.github.com>
2020-08-15 19:00:38 +0800

    完善WinAPI颜色管理方案

--- include/WinAPI_control.hpp
+++ include/WinAPI_control.hpp
@@ -7,10 +7,8 @@ void moveleft(int i);
 void movedown(int i);
 void moveup(int i);
 void clean_screen();
-void blue_foreground();
-void yellow_foreground();
-void yellow_background();
-void end_all();
-bool OpenANSIControlChar();
+void Set_Default_color(int r,int g,int b,int f_r,int f_g,int f_b);
+void color(int c);
+void Reset_color();
 //请确保在调用cursormove后任意的输出都记录下来
 extern int cursor_location;
