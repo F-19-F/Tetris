c2d7326012c36e416c3c697b9e9a0bba119e4a96
F-19-F <58457605+F-19-F@users.noreply.github.com>
2020-08-27 19:30:41 +0800

    格式化所有源文件

--- include/WinAPI_control.hpp
+++ include/WinAPI_control.hpp
@@ -1,6 +1,6 @@
 void Win_Required();
 void Reset_Win_Required();
-void cursor_move(int x,int y);
+void cursor_move(int x, int y);
 void hide_cursor();
 void dishide_cusor();
 void moveright(int i);
@@ -8,7 +8,7 @@ void moveleft(int i);
 void movedown(int i);
 void moveup(int i);
 void clean_screen();
-void Set_Default_color(int r,int g,int b,int f_r,int f_g,int f_b);
+void Set_Default_color(int r, int g, int b, int f_r, int f_g, int f_b);
 void color(int c);
 void Reset_color();
 //请确保在调用cursormove后任意的输出都记录下来
