fed073e3e4cc4907c89831b83260496fc5a27529
F-19-F <58457605+F-19-F@users.noreply.github.com>
2020-09-10 17:13:32 +0800

    小更新

--- include/WinAPI_control.hpp
+++ include/WinAPI_control.hpp
@@ -13,6 +13,7 @@ void clean_screen();
 void Set_Default_color(int r, int g, int b, int f_r, int f_g, int f_b);
 void color(int c);
 void Reset_color();
+void Hide_File(char* path);
 //请确保在调用cursormove后任意的输出都记录下来
 extern int cursor_location;
 #endif
