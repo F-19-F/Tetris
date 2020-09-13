5cd7af51d912d31181a3ebcc6a6b844acd216a85
F-19-F <58457605+F-19-F@users.noreply.github.com>
2020-09-13 22:59:36 +0800

    更新换行机制

--- include/UI.hpp
+++ include/UI.hpp
@@ -2,7 +2,8 @@
 #define _TUI
 #include "Keydec.hpp"
 #include "Size.hpp"
-#define About "这是一个C++俄罗斯方块游戏\n具有以下特征\n   1.游戏运行过程中途退出再次打开自动恢复\n   2.大小自适应\n   3.实时按键反馈"
+#define About "这是一个C++俄罗斯方块游戏\n具有以下特征\n   1.游戏运行过程中途退出再次打开自动恢复\n   2.大小自适应\n   3.实时按键反馈\
+\n \n By SWU-F19"
 #ifdef _WIN32
 #define Title                              \
 	cursor_move(x, y);                     \
